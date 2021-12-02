2149bcdd4619ab1aefee17b85022d8ed74cfe805
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2021-12-02 18:59:42 +0530

    Switchcase

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -6,7 +6,7 @@ namespace Employee
     {
         public static void Main()
         {
-            Console.Write("*******Calculating Part Time Employee Wage:********\n");
+            Console.Write("*******Calculating Part Time Employee Wage Using Switch Case:********\n");
             EmployeeCalculation empWageCalculation = new EmployeeCalculation();
             empWageCalculation.Employee();
 
