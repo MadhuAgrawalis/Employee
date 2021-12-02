107df357dd971c346741b901a1a7581175e63ad5
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2021-12-02 17:59:22 +0530

    presentAbsent

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Employee
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+                int Fulltime = 1;
+                Random random_num = new Random();
+                int checkemp = random_num.Next(0, 2);
+                if (checkemp == Fulltime)
+                {
+                    Console.WriteLine("Employee Is Present");
+                }
+                else
+                {
+                    Console.Write("Employee is Absent");
+                }
+
+
+            
         }
     }
 }
